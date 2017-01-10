962efc7cbbf024f1bce01b25f6ea7bdb5087178b
Carlos Rodrigues <carlos.all.mail@gmail.com>
2017-01-10 07:21:11 -0700

    Added comments and snake-case

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const body_parser = require('body-parser');
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-// /?loggedin=true
+// First tell express that we want to use body-parser to convert form/json data into JS objects
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 
+// Calls without routes will be executed for every request
+// Will encounter this middleware first, because it is before the others
+// Add this to you URL get passed this middleware: /?loggedin=true
 app.use(function(req, res, next) {
   console.log('Hello from first the middleware!');
   if(req.query.loggedin) {
@@ -26,12 +29,16 @@ app.use(function(req, res, next) {
   next();
 });
 
+// Here our function is the middleware
+// We are saying run this function when this GET route happens
 app.get('/', (req, res) => {
   console.log('Hello from inside the route');
   throw new Error('WOAHHHH!!!');
   res.send('Hello World 1!');
 });
 
+// If it gets here without being handled by one of our other middleware above here,
+// Then this executes.  Here is normally where we would put our 404 handling
 app.use(function(req, res, next) {
   console.log('Route not found');
   res.send('NOT FOUND!!!!');
